26e0b717e8132562812bf86a8f9b59e78c8b2d59
clapathiotis <43289218+clapathiotis@users.noreply.github.com>
2022-01-17 19:53:37 +0100

    fixed

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
+from jbi100_app.data import get_data
 from jbi100_app.main import app
 from jbi100_app.views.menu import make_menu_layout
 from jbi100_app.views.scatterplot import Scatterplot
-import pandas as pd
 
 from dash import html
 import plotly.express as px
@@ -10,14 +10,8 @@ from dash.dependencies import Input, Output
 
 if __name__ == '__main__':
     # Create data
-    
-    df = pd.read_csv(r'decoded-dft-road-casualty-statistics-accident-2020.csv', delimiter=',')
-
-    df = df[df["number_of_casualties"] > 1]
-    df = df[df["weather_conditions"] != 9]
-    df = df[df["weather_conditions"] != "Uknown"]
-    df = df[df["weather_conditions"] != 8]
-    
+    df = get_data()
+    print(df.head())
     # Instantiate custom views
     scatterplot1 = Scatterplot("Scatterplot 1", 'weather_conditions', 'number_of_casualties', df)
     scatterplot2 = Scatterplot("Scatterplot 2", 'light_conditions', 'number_of_casualties', df)
@@ -47,19 +41,19 @@ if __name__ == '__main__':
     # Define interactions
     @app.callback(
         Output(scatterplot1.html_id, "figure"), [
-        Input("select-area-scatter-1", "value"),
+        Input("select-color-scatter-1", "value"),
         Input(scatterplot2.html_id, 'selectedData')
     ])
-    def update_scatter_1(selected_area, selected_data):
-        return scatterplot1.update(selected_area, selected_data)
+    def update_scatter_1(selected_color, selected_data):
+        return scatterplot1.update(selected_color, selected_data)
 
     @app.callback(
         Output(scatterplot2.html_id, "figure"), [
-        Input("select-area-scatter-2", "value"),
+        Input("select-color-scatter-2", "value"),
         Input(scatterplot1.html_id, 'selectedData')
     ])
-    def update_scatter_2(selected_area, selected_data):
-        return scatterplot2.update(selected_area, selected_data)
+    def update_scatter_2(selected_color, selected_data):
+        return scatterplot2.update(selected_color, selected_data)
 
 
     app.run_server(debug=False, dev_tools_ui=False)
--- jbi100_app/data.py
+++ jbi100_app/data.py
@@ -1,14 +1,12 @@
-import plotly.express as pf
-import csv
+import plotly.express as px
 import pandas as pd
-
 
 def get_data():
     # Read data
-    df = pd.read_csv(r'dataset.csv', delimiter=',')
-  
+    df = pd.read_csv('decoded-dft-road-casualty-statistics-accident-2020.csv')
 
-    # Any further data preprocessing can go here
-    
+    # Any further data preprocessing can go her
+    df = df[df["number_of_casualties"]>1]
+    df = df[df["weather_conditions"] != "Other"]
+    df = df[df["weather_conditions"] != "Unknown"]
     return df
-
